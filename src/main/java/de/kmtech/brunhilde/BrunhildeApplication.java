992176904068731a228d8cc44e71d08ab45ea69f
Kilian Mauer <kilian.mauer@buchhalter.pro>
2020-10-06 19:56:12 +0200

    includes scaffolding of spring set up

---
+++ src/main/java/de/kmtech/brunhilde/BrunhildeApplication.java
@@ -0,0 +1,13 @@
+package de.kmtech.brunhilde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrunhildeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrunhildeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/kmtech/brunhilde/BrunhildeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kmtech.brunhilde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrunhildeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
